5a57235f3f455cd1914adedd620f2c91b3f265c6
iainrwolfheart <iainrgorrie@gmail.com>
2021-06-10 21:05:07 +0100

    JWT Commenced. successfulAuthentication method tbc.

---
+++ src/main/java/com/project/asidesappbe/jwt/JwtConfig.java
@@ -0,0 +1,4 @@
+package com.project.asidesappbe.jwt;
+
+public class JwtConfig {
+}
---
+++ src/main/java/com/project/asidesappbe/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.project.asidesappbe.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfig jwtConfig;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager,
+                                                      AuthenticationManager authenticationManager1,
+                                                      JwtConfig jwtConfig) {
+        super(authenticationManager);
+        this.authenticationManager = authenticationManager1;
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+            Authentication authenticationAttempt = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()
+            );
+
+            return authenticationManager.authenticate(authenticationAttempt);
+
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
