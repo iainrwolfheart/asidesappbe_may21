c59a28402b979228db431964079e0b7f975712d3
iainrwolfheart <iainrgorrie@gmail.com>
2021-06-12 16:04:09 +0100

    jwt properties set active in app.props, setters added to JwtConfig to make them actually work. Pseudocode added to Jwt...Verifier for next steps

--- src/main/java/com/project/asidesappbe/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/project/asidesappbe/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.project.asidesappbe.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -48,6 +51,18 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult)
             throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+
+        String jwt = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date
+                        .valueOf(LocalDate.now()
+                                .plusDays(jwtConfig.getTokenValidityTimeInDays())))
+                .signWith(jwtConfig.getSecretKeySha())
+                .compact();
+
+        response.addHeader(jwtConfig.getAuthorizationHeader(),
+                jwtConfig.getBearerPrefix() + jwt);
     }
 }
